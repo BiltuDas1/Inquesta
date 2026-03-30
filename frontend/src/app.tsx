3c8c477e442769a5ed78392d3c82bca4bd14ba19
BiltuDas1 <billionto@gmail.com>
2026-03-30 23:32:09 +0530

    feat: Added courses page

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import Home from "./pages/homepage";
 import Register from "./pages/register";
+import Courses from "./pages/courses";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/register" element={<Register />} />
+        <Route path="/courses" element={<Courses />} />
         <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
