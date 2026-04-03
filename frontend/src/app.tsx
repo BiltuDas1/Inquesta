36086b28045b8cb1e48084a1efae68c4e06e99ca
BiltuDas1 <billionto@gmail.com>
2026-04-03 23:29:17 +0530

    feat: Removed extra code

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,14 +1,10 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import Home from "./pages/homepage";
-import Register from "./pages/register";
-import Courses from "./pages/courses";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/register" element={<Register />} />
-        <Route path="/courses" element={<Courses />} />
         <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
