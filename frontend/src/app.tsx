4a3f35ad3a164e06a7071a6b2c35fede6e9cb2ee
Santu pramanik <santu700141@gmail.com>
2026-04-04 13:01:38 +0530

    chore: configure tailwind css

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import Home from "./pages/homepage";
+import {SignupPage} from "./pages/SignupPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<SignupPage />} />
       </Routes>
     </BrowserRouter>
   )
