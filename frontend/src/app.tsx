56fec7041f7e79e3b9862d32c8e422107f193256
Santu pramanik <santu700141@gmail.com>
2026-04-06 13:02:08 +0530

    feat: added login page

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import Home from "./pages/homepage";
 import SignupPage from "./pages/SignupPage";
-// import LoginPage from "./pages/LoginPage";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
@@ -9,7 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/signup" element={<SignupPage />} />
-        {/* <Route path="/login" element={<LoginPage/>}/> */}
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </BrowserRouter>
   );
