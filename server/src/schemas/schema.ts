c11c39b44fb131b5f64f4da173fcf174c131a29e
BiltuDas1 <billionto@gmail.com>
2026-04-09 20:22:08 +0530

    refactor: Removed the restart code

--- server/src/schemas/schema.ts
+++ server/src/schemas/schema.ts
@@ -11,37 +11,10 @@ export const schema = createSchema<FastifyContext>({
     type Query {
       hello: String
     }
-    type Mutation {
-      restartServer(secret: String!): String
-    }
   `,
   resolvers: {
     Query: {
-      hello: () => "Hello World!",
-    },
-    Mutation: {
-      restartServer: (_parent, args) => {
-        // Check if the "STAGING" environment variable exists at all
-        // If it doesn't, this feature is completely disabled.
-        const isStaging = process.env.STAGING !== undefined;
-
-        if (!isStaging) {
-          throw new Error("This action is only available in the Staging environment.");
-        }
-
-        // Secondary check: Security Secret
-        if (args.secret !== process.env.RESTART_SECRET) {
-          throw new Error("Invalid restart secret.");
-        }
-
-        // Trigger the reboot
-        setTimeout(() => {
-          console.log("Staging restart triggered. Powering down...");
-          process.exit(0); 
-        }, 1000);
-
-        return "Reboot request received";
-      },
+      hello: () => "Hello from Fastify + Yoga!",
     },
   },
 });
