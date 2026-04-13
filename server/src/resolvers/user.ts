916504328dc44aeb2c0e0f40ce5b6d4324098801
BiltuDas1 <billionto@gmail.com>
2026-04-13 23:26:47 +0530

    feat: Get role on login

--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -1,7 +1,7 @@
 import { hash, verify } from "argon2";
 import { db } from "../config.ts";
 import { users } from "../databases/schema.ts";
-import type { User } from "../types/user.ts";
+import { type UserRole, type User } from "../types/user.ts";
 import { and, eq } from "drizzle-orm";
 
 export async function registerUser(data: User) {
@@ -10,18 +10,27 @@ export async function registerUser(data: User) {
   return true;
 }
 
-export async function loginUser(email: string, password: string) {
+export async function loginUser(
+  email: string,
+  password: string,
+): Promise<UserRole | false> {
   const [userRecord] = await db
-    .selectDistinct({ password: users.password })
+    .selectDistinct({ password: users.password, role: users.role })
     .from(users)
     .where(and(eq(users.isActive, true), eq(users.email, email)))
     .limit(1);
-
 
   if (!userRecord) {
     return false;
   }
 
   const isCorrect = await verify(userRecord.password, password);
-  return isCorrect;
+  if (!isCorrect) {
+    return false;
+  }
+
+  return {
+    email: email,
+    role: userRecord.role,
+  };
 }
