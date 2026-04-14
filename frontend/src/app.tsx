b49aeb64082bd5ea19f8067802b2dd4d061536af
Santu pramanik <santu700141@gmail.com>
2026-04-14 13:15:27 +0530

    feat: integrate login page with GraphQL login query and handle user state

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -4,16 +4,28 @@ import SignupPage from "./pages/signuppage";
 import LoginPage from "./pages/loginpage";
 import DashboardPage from "./pages/dashboardpage";
 import CoursePage from "./pages/coursepage";
+import ProtectedRoute from "./components/middleware/protectedroute";
+import PublicRoute from "./components/middleware/publicroute";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+
         <Route path="/" element={<Home />} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/dashboard" element={<DashboardPage></DashboardPage>}/>
-        <Route path="/course" element={<CoursePage></CoursePage>}/>
+        <Route path="/courses" element={<CoursePage></CoursePage>} />
+
+        {/* Block if already logged in */}
+        <Route element={<PublicRoute />}>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+        </Route>
+
+        {/* Protected Route */}
+        <Route element={<ProtectedRoute />}>
+          <Route path="/dashboard" element={<DashboardPage></DashboardPage>} />
+        </Route>
+
       </Routes>
     </BrowserRouter>
   );
