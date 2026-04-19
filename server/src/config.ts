2c3d9defc9a3b2824fc316ed06a6ed97a66f1747
BiltuDas1 <billionto@gmail.com>
2026-04-19 23:21:30 +0530

    feat: Converted EDDSA_PRIVATE_KEY Environment to base64

--- server/src/config.ts
+++ server/src/config.ts
@@ -5,6 +5,8 @@ import { Email } from "./utils/email.ts";
 import { Template } from "./utils/template.ts";
 import { createClient } from "redis";
 import { OAuth2Client } from "google-auth-library";
+import { importPKCS8, importSPKI } from "jose";
+import * as crypto from "crypto";
 
 export const isProduction = process.env.PRODUCTION !== undefined;
 export const serverConfig = {
@@ -36,10 +38,24 @@ redis.on("error", (err) => console.error("Redis Client Error", err));
 // JWT Token Expiry
 export const ACCESS_TOKEN_EXPIRY = 900;
 export const REFRESH_TOKEN_EXPIRY = 3600 * 24 * 30;
-export const EDDSA_PRIVATE_KEY = requireEnv("EDDSA_PRIVATE_KEY");
+// export const EDDSA_PRIVATE_KEY = requireEnv("EDDSA_PRIVATE_KEY");
 
 // Google Authentication
 export const GOOGLE_CLIENT = new OAuth2Client({
   clientId: requireEnv("GOOGLE_CLIENT_ID"),
   clientSecret: requireEnv("GOOGLE_CLIENT_SECRET")
 });
+
+// EdDSA Keys
+export let EDDSA_PRIVATE_KEY: CryptoKey
+export let EDDSA_PUBLIC_KEY: CryptoKey
+
+export async function loadEdDSAKey() {
+  const private_key_str = Buffer.from(requireEnv("EDDSA_PRIVATE_KEY"), "base64").toString("utf-8");
+  EDDSA_PRIVATE_KEY = await importPKCS8(private_key_str, "EdDSA");
+  const public_key_str = crypto.createPublicKey(private_key_str).export({
+    type: "spki",
+    format: "pem",
+  });
+  EDDSA_PUBLIC_KEY = await importSPKI(public_key_str, "EdDSA");
+}
