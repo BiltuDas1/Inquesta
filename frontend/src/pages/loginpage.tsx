c3ccf2b30e28801b8b29c6d6b8857cf494758025
Santu pramanik <santu700141@gmail.com>
2026-04-21 21:18:43 +0530

    fix: typescript unused react state error

--- frontend/src/pages/loginpage.tsx
+++ frontend/src/pages/loginpage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, {  useState } from "react";
 import InputField from "../components/ui/inputfield";
 import { gql } from "@apollo/client";
 import { useLazyQuery } from "@apollo/client/react";
