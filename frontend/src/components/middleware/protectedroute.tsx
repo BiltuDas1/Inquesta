f59e77e10bf76499278357d9a9cdfc04efea0994
Santu pramanik <santu700141@gmail.com>
2026-05-06 01:39:40 +0530

    fix: changes isauth

--- frontend/src/components/middleware/protectedroute.tsx
+++ frontend/src/components/middleware/protectedroute.tsx
@@ -1,14 +1,27 @@
 import { Navigate, Outlet } from "react-router";
+import { useAuth } from "../../context/authcontext";
+
 const ProtectedRoute = () => {
-  const user = JSON.parse(localStorage.getItem("user") || "null");
+
+  const { user, isLoading } = useAuth();
+
+  if (isLoading) {
+    return (
+      <div className="min-h-screen bg-[#10141a] flex items-center justify-center text-[#6fffd9] font-body">
+        Authenticating...
+      </div>
+    );
+  }
+
 
   if (!user) {
     return <Navigate to="/login" replace />;
   }
 
   if (user.role !== "admin") {
-    return <Navigate to="/courses" replace />; // or /unauthorized
+    return <Navigate to="/courses" replace />;
   }
+
 
   return <Outlet />;
 };
