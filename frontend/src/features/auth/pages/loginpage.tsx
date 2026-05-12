2ea4be3ffc224d11fd45f91aa2408e7e2898cb2d
Santu pramanik <santu700141@gmail.com>
2026-05-13 00:08:36 +0530

    fix: changes the fonts style and login query into login mutation

--- frontend/src/features/auth/pages/loginpage.tsx
+++ frontend/src/features/auth/pages/loginpage.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { gql } from "@apollo/client";
-import { useLazyQuery } from "@apollo/client/react";
+import { useLazyQuery, useMutation } from "@apollo/client/react";
 import { useNavigate } from "react-router";
 import { google_login } from "../utils/googleauth";
 import toast from "react-hot-toast";
@@ -9,8 +9,8 @@ import GoogleSVG from "../../../shared/svg/google";
 import InputField from "../../../shared/components/inputfield";
 
 // QUERY to get user data
-const LOGIN_QUERY = gql`
-  query login($email: String!, $password: String!) {
+const LOGIN_MUTATION = gql`
+  mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
       data {
         firstname
@@ -68,7 +68,7 @@ export default function LoginPage() {
   const navigate = useNavigate();
 
   // Initialize Apollo Lazy Query
-  const [loginUser, { loading, error }] = useLazyQuery<LoginData>(LOGIN_QUERY);
+  const [loginUser, { loading, error }] = useMutation<LoginData>(LOGIN_MUTATION);
   const [fetchUserInfo] = useLazyQuery<UserInfoData>(GET_USER_INFO, {
     fetchPolicy: "network-only",
   });
@@ -157,7 +157,7 @@ export default function LoginPage() {
     );
 
   return (
-    <div className="h-screen overflow-hidden flex bg-[#0a1515] font-sans">
+    <div className="h-screen overflow-hidden flex bg-[#0a1515] font-headline">
       {/* LEFT PANEL */}
       <div
         className="hidden lg:flex lg:w-[52%] relative flex-col justify-center items-center px-10 py-8 overflow-hidden"
