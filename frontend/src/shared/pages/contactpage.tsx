f38ad76369239ddea9cd2e561233929c489d3a51
Santu pramanik <santu700141@gmail.com>
2026-05-21 11:07:24 +0530

    fix: remove unused variable and commented code also add carousel

--- frontend/src/shared/pages/contactpage.tsx
+++ frontend/src/shared/pages/contactpage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 
 export default function ContactPage() {
   const [inquiryType, setInquiryType] = useState("General Inquiry");
