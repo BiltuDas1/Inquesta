92f2afc198a9c95872052e99bdef45ed1c339c17
Indranil65 <indranilpramanik018@gmail.com>
2026-06-02 11:02:32 +0530

    test: Added parent login successful test cases

--- testing/graphql/test_loginuser.py
+++ testing/graphql/test_loginuser.py
@@ -1,7 +1,7 @@
 from playwright.sync_api import APIRequestContext
 
 
-def test_login_successful(api_request_context: APIRequestContext):
+def test_student_login_successful(api_request_context: APIRequestContext):
 
   register_response = api_request_context.post(
     "/",
@@ -106,6 +106,113 @@ def test_login_successful(api_request_context: APIRequestContext):
   assert login_result["data"]["firstname"] == "rohan"
   assert login_result["data"]["lastname"] == "manna"
   assert login_result["data"]["role"] == "student"
+
+
+def test_parent_login_successful(api_request_context: APIRequestContext):
+
+  register_response = api_request_context.post(
+    "/",
+    data={
+      "query": """
+                mutation Register(
+                  $email: String!
+                  $firstname: String!
+                  $is_student: Boolean!
+                  $lastname: String
+                  $password: String!
+                ) {
+                  register(
+                    email: $email
+                    firstname: $firstname
+                    is_student: $is_student
+                    lastname: $lastname
+                    password: $password
+                  ) {
+                    success
+                    message
+                  }
+                }
+            """,
+      "variables": {
+        "email": "rohan123@gmail.com",
+        "firstname": "rohan",
+        "is_student": False,
+        "lastname": "manna",
+        "password": "Pass@123",
+      },
+    },
+  )
+
+  assert register_response.ok, (
+    f"Register API failed with status {register_response.status}"
+  )
+  register_json = register_response.json()
+
+  assert "errors" not in register_json, (
+    f"Register GraphQL Errors: {register_json['errors']}"
+  )
+
+  assert "data" in register_json, "Register response missing 'data' field"
+  register_result = register_json["data"].get("register")
+
+  assert register_result is not None, (
+    f"Register result was null. Errors: {register_json.get('errors')}"
+  )
+  assert register_result.get("success") is True, (
+    f"Expected True, got {register_result.get('success')}"
+  )
+  assert isinstance(register_result.get("message"), str), (
+    f"Expected str message, got {type(register_result.get('message'))}"
+  )
+
+  login_response = api_request_context.post(
+    "/",
+    data={
+      "query": """
+                mutation Login($email: String!, $password: String!) {
+                  login(email: $email, password: $password) {
+                    success
+                    message
+                    data {
+                      email
+                      firstname
+                      lastname
+                      role
+                    }
+                  }
+                }
+            """,
+      "variables": {
+        "email": "rohan123@gmail.com",
+        "password": "Pass@123",
+      },
+    },
+  )
+
+  assert login_response.ok, f"Login API failed with status {login_response.status}"
+  login_json = login_response.json()
+
+  assert "errors" not in login_json, f"Register GraphQL Errors: {login_json['errors']}"
+
+  assert "data" in login_json, "Login response missing 'data' field"
+  login_result = login_json["data"].get("login")
+
+  assert login_result is not None, (
+    f"Login result was null. Errors: {login_json.get('errors')}"
+  )
+  assert login_result.get("success") is True, (
+    f"Expected True, got {login_result.get('success')}"
+  )
+  assert isinstance(login_result.get("message"), str), (
+    f"Expected str message, got {type(login_result.get('message'))}"
+  )
+  assert login_result.get("data") is not None, (
+    f"Expected null, got {login_result.get('data')}"
+  )
+  assert login_result["data"]["email"] == "rohan123@gmail.com"
+  assert login_result["data"]["firstname"] == "rohan"
+  assert login_result["data"]["lastname"] == "manna"
+  assert login_result["data"]["role"] == "parent"
 
 
 def test_login_email_not_exist(api_request_context: APIRequestContext):
